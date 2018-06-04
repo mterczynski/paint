dd0f8e179f23d91302ad28d3003c55ed618e19d8
Michał Terczyński <mterczynski271@gmail.com>
2018-06-04 14:44:14 +0200

    Updated tools component

--- src/components/ribbon/main-tools-tab/groups/tools/Tools.js
+++ src/components/ribbon/main-tools-tab/groups/tools/Tools.js
@@ -7,50 +7,58 @@ require('./Tools.scss');
 
 const mapStateToProps = (state) => {
 	return { 
-		
+		selectedTool: state.selectedTool
 	};
 };
   
 class ToolsComponent extends React.Component{
 
+	selectTool(toolId){
+		store.dispatch(actions.selectTool(toolId));
+	}
+
 	constructor(props) {
 		super(props)
-		this.state = {
-			isListCollapsed: false
-		}
+		// this.state = {}
 	}
 
 	render(){
 		return <div className="Tools">
 			<div className="Tools__iconRow">
-				<div className="Tools__icon-1">
+				<div onClick={()=>this.selectTool(1)}
+				className={"Tools__icon-1 " + (this.props.selectedTool == 1 ? 'Tools__icon--active' : '')}>
 					<img className="Tools__iconImage" draggable="false"
 					src={require('../../../../../assets/icons/main-tools-tab/3_tools/1.png')} alt=""/>
 				</div>
 				
-				<div className="Tools__icon-2">
+				<div onClick={()=>this.selectTool(2)}
+				className={"Tools__icon-2 " + (this.props.selectedTool == 2 ? 'Tools__icon--active' : '')}>
 					<img className="Tools__iconImage" draggable="false"
 					src={require('../../../../../assets/icons/main-tools-tab/3_tools/2.png')} alt=""/>
 				</div>
 
-				<div className="Tools__icon-3">
+				<div onClick={()=>this.selectTool(3)}
+				className={"Tools__icon-3 " + (this.props.selectedTool == 3 ? 'Tools__icon--active' : '')}>
 					<img className="Tools__iconImage" draggable="false"
 					src={require('../../../../../assets/icons/main-tools-tab/3_tools/3.png')} alt=""/>
 				</div>
 			</div>
 
 			<div className="Tools__iconRow">
-				<div className="Tools__icon-1">
+				<div onClick={()=>this.selectTool(4)}
+				className={"Tools__icon-1 " + (this.props.selectedTool == 4 ? 'Tools__icon--active' : '')}>
 					<img className="Tools__iconImage" draggable="false"
 					src={require('../../../../../assets/icons/main-tools-tab/3_tools/4.png')} alt=""/>
 				</div>
 
-				<div className="Tools__icon-2">
+				<div onClick={()=>this.selectTool(5)}
+				className={"Tools__icon-2 " + (this.props.selectedTool == 5 ? 'Tools__icon--active' : '')}>
 					<img className="Tools__iconImage" draggable="false"
 					src={require('../../../../../assets/icons/main-tools-tab/3_tools/5.png')} alt=""/>
 				</div>
 
-				<div className="Tools__icon-3">
+				<div onClick={()=>this.selectTool(6)}
+				className={"Tools__icon-3 " + (this.props.selectedTool == 6 ? 'Tools__icon--active' : '')}>
 					<img className="Tools__iconImage" draggable="false"
 					src={require('../../../../../assets/icons/main-tools-tab/3_tools/6.png')} alt=""/>
 				</div>
