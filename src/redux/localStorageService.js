9bcc08a08fc22e87f8d76f737db3747747a97e69
Michał Terczyński <mterczynski271@gmail.com>
2018-12-01 02:32:53 +0100

    refactored localStorageService.js

--- src/redux/localStorageService.js
+++ src/redux/localStorageService.js
@@ -10,36 +10,34 @@
 
 
 export const localStorageService = {
-	prefix: '_10D@9dF_msPaint_', // unique prefix for localstorage, should be '' on production
+	prefix: '_10D@9dF_msPaint_', // unique prefix for localstorage, could be '' on production
 
-	save(name, value){
+	save(name, value) {
 		localStorage.setItem(prefix + name, JSON.stringify(value));
 	},
 
-	read(name){
+	read(name) {
 		return JSON.parse(localStorage.getItem(prefix + name));
 	},
 
-	init(){
-		if(!localStorageService.read('viewTab__bottomBar')){
-			localStorageService.save('viewTab__bottomBar', true);
-		}
-		if(!localStorageService.read('viewTab__gridLines')){
-			localStorageService.save('viewTab__gridLines', false);
-		}
-		if(!localStorageService.read('viewTab__rulers')){
-			localStorageService.save('viewTab__rulers', false);
-		}
-		if(!localStorageService.read('isRibbonAlwaysCollapsed')){
-			localStorageService.save('isRibbonAlwaysCollapsed', false);
+	initItem(itemName, value) {
+		if (!localStorageService.read(itemName)) {
+			localStorageService.save(itemName, value);
 		}
 	},
 
-	getAppData(){
+	init() {
+		localStorageService.initItem('viewTab__bottomBar', true);
+		localStorageService.initItem('viewTab__gridLines', false);
+		localStorageService.initItem('viewTab__rulers', false);
+		localStorageService.initItem('isRibbonAlwaysCollapsed', false);
+	},
+
+	getAppData() {
 		localStorageService.init();
 
 		return {
-			viewTab:{
+			viewTab: {
 				rulers: localStorageService.read('viewTab__rulers'),
 				bottomBar: localStorageService.read('viewTab__bottomBar'),
 				gridLines: localStorageService.read('viewTab__gridLines')
