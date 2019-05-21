b5eb219d83eac077e1a9595f97030883bbf2b358
Michal Terczynski <michal.terczynski@globallogic.com>
2019-05-21 14:16:51 +0200

    fixed webpack and babel errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,41 @@
+const path = require('path');
+
 module.exports = {
-		entry: './src/App.js',
-		output: {
-			filename: 'bundle.js'
-		},
-		mode: 'development',
-		module: {
-			rules: [
-				{
-					test: /\.css$/,
-					use: ['style-loader', 'css-loader']
-				},
-				{
-					test: /\.scss$/,
-					use: [
-						"style-loader", // creates style nodes from JS strings
-						"css-loader", // translates CSS into CommonJS
-						"sass-loader" // compiles Sass to CSS
-					]
-				},
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					use: {
-						loader: 'babel-loader'
-					}
-				},
-				{
-					test: /\.(png|jpg|PNG)$/,
-					loader: 'url-loader'
+	entry: './src/App.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/dist'
+	},
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.scss$/,
+				use: [
+					"style-loader", // creates style nodes from JS strings
+					"css-loader", // translates CSS into CommonJS
+					"sass-loader" // compiles Sass to CSS
+				]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
 				}
-			]
-		}
+			},
+			{
+				test: /\.(png|jpg|PNG)$/,
+				loader: 'url-loader'
+			}
+		]
+	},
+	watchOptions: {
+		ignored: /node_modules/
+	}
 }
