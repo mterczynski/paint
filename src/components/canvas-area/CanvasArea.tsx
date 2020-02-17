77e0e83627e6e7218be5a38d61e6f21c28f7397e
Michal Terczynski <mterczynski271@gmail.com>
2020-02-17 21:25:46 +0100

    Added pencil cursor to canvas

--- src/components/canvas-area/CanvasArea.tsx
+++ src/components/canvas-area/CanvasArea.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+import { AppState, AvailableTools } from '../../types';
 import './CanvasArea.scss';
 
-const CanvasArea = () => (
-	<div className='CanvasArea'>
-		<canvas className='CanvasArea__canvas' width='500' height='500' />
-	</div>
-);
+const cursors = {
+	[AvailableTools.Pencil]: `url(${require('../../assets/cursors/pencil.png')}), auto`,
+	[AvailableTools.None]: 'default',
+};
+
+const CanvasArea = () => {
+	const selectedTool = useSelector((state: AppState) => state.selectedTool);
+
+	const cursor: string = cursors[selectedTool];
+
+	return (
+		<div className='CanvasArea'>
+			<canvas className='CanvasArea__canvas' width='500' height='500' style={{cursor}}/>
+		</div>
+	);
+};
 
 export default CanvasArea;
