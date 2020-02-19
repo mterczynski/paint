42509c3d298360614c51cc0e8f4011112338aef9
Michal Terczynski <mterczynski271@gmail.com>
2020-02-19 19:23:02 +0100

    Refactored Shapes.tsx

--- src/components/ribbon/main-tools-tab/groups/shapes/Shapes.tsx
+++ src/components/ribbon/main-tools-tab/groups/shapes/Shapes.tsx
@@ -38,49 +38,47 @@ const shapeDescriptions = Object.freeze([
 	'Błyskawica',
 ]);
 
-class Shapes extends React.Component {
-	render() {
-		// images:
-		const arrDown = require('../../../../../assets/icons/arrow_down.png');
-		const contourActive = require('../../../../../assets/icons/main-tools-tab/5_shapes/contour_active.png');
-		const fill = require('../../../../../assets/icons/main-tools-tab/5_shapes/fill_active.png');
+const Shapes = () => {
+	// images:
+	const arrDown = require('../../../../../assets/icons/arrow_down.png');
+	const contourActive = require('../../../../../assets/icons/main-tools-tab/5_shapes/contour_active.png');
+	const fill = require('../../../../../assets/icons/main-tools-tab/5_shapes/fill_active.png');
 
-		return (
-			<div className='Shapes'>
-				<div className='Shapes__content'>
-					<div className='Shapes__shapeList'>
-						<div className='Shapes__shapeListContent'>
-							{shapeImageUrls.map((shape, i) => (
-								<img
-									draggable='false'
-									className='Shapes__shape'
-									src={shape}
-									alt=''
-									key={i}
-								/>
-							))}
-						</div>
-						<div className='Shapes__shapeListControls' />
+	return (
+		<div className='Shapes'>
+			<div className='Shapes__content'>
+				<div className='Shapes__shapeList'>
+					<div className='Shapes__shapeListContent'>
+						{shapeImageUrls.map((shape, i) => (
+							<img
+								draggable='false'
+								className='Shapes__shape'
+								src={shape}
+								alt=''
+								key={i}
+							/>
+						))}
+					</div>
+					<div className='Shapes__shapeListControls' />
+				</div>
+
+				<div className='Shapes__options'>
+					<div className='Shapes__option Shapes__option--first'>
+						<img draggable='false' src={contourActive} alt='' />
+						<span> Kontur </span>
+						<img draggable='false' alt='' src={arrDown} />
 					</div>
 
-					<div className='Shapes__options'>
-						<div className='Shapes__option Shapes__option--first'>
-							<img draggable='false' src={contourActive} alt='' />
-							<span> Kontur </span>
-							<img draggable='false' alt='' src={arrDown} />
-						</div>
-
-						<div className='Shapes__option Shapes__option--second'>
-							<img draggable='false' src={fill} alt='' />
-							<span> Wypełnienie </span>
-							<img draggable='false' alt='' src={arrDown} />
-						</div>
+					<div className='Shapes__option Shapes__option--second'>
+						<img draggable='false' src={fill} alt='' />
+						<span> Wypełnienie </span>
+						<img draggable='false' alt='' src={arrDown} />
 					</div>
 				</div>
-				<div className='Shapes__description'>Kształty</div>
 			</div>
-		);
-	}
-}
+			<div className='Shapes__description'>Kształty</div>
+		</div>
+	);
+};
 
 export default Shapes;
