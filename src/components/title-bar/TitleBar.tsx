01314e2651642be4183b5f0fab1abb78f19a3039
Michal Terczynski <mterczynski271@gmail.com>
2020-02-19 19:02:51 +0100

    Refactored TitleBar

--- src/components/title-bar/TitleBar.tsx
+++ src/components/title-bar/TitleBar.tsx
@@ -1,56 +1,42 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import * as actions from '../../redux/actions';
 import store from '../../redux/store';
 
 import './TitleBar.scss';
 
-const mapStateToProps = state => {
-	return { };
-};
-
-class TitleBarComponent extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
-
-	maximizeWindow = () => {
-		store.dispatch(actions.maximize());
-	}
-
-	render() {
-		return (
-			<div className='TitleBar'>
-				<div className='TitleBar__window-icons'>
-					<div className='TitleBar__window-icons__icon TitleBar__window-icons__icon__minimizeTile'>
-						<div className='TitleBar__window-icons__icon__minimize' />
-					</div>
-
-					<div
-						className='TitleBar__window-icons__icon'
-						onClick={this.maximizeWindow}
-					>
-						<img
-							draggable='false'
-							src={require('./../../assets/icons/top/maximize.png')}
-							className='TitleBar__window-icons__icon__image'
-						/>
-					</div>
-
-					<div className='TitleBar__window-icons__icon TitleBar__window-icons__icon--close'>
-						<img
-							draggable='false'
-							src={require('./../../assets/icons/top/close.png')}
-							className='TitleBar__window-icons__icon__image'
-						/>
-					</div>
-				</div>
-			</div>
-		);
-	}
+function maximizeWindow() {
+	store.dispatch(actions.maximize());
 }
 
-const TitleBar = connect(mapStateToProps)(TitleBarComponent);
+const TitleBar = () => {
+	return (
+		<div className='TitleBar'>
+			<div className='TitleBar__window-icons'>
+				<div className='TitleBar__window-icons__icon TitleBar__window-icons__icon__minimizeTile'>
+					<div className='TitleBar__window-icons__icon__minimize' />
+				</div>
+
+				<div
+					className='TitleBar__window-icons__icon'
+					onClick={maximizeWindow}
+				>
+					<img
+						draggable='false'
+						src={require('./../../assets/icons/top/maximize.png')}
+						className='TitleBar__window-icons__icon__image'
+					/>
+				</div>
+
+				<div className='TitleBar__window-icons__icon TitleBar__window-icons__icon--close'>
+					<img
+						draggable='false'
+						src={require('./../../assets/icons/top/close.png')}
+						className='TitleBar__window-icons__icon__image'
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default TitleBar;
