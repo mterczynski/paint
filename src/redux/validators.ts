ccfc4b87efedcaebd5168a5ce549e5569fb19811
Michal Terczynski <mterczynski271@gmail.com>
2020-02-19 18:56:18 +0100

    Upgraded mterczynski-tslint

--- src/redux/validators.ts
+++ src/redux/validators.ts
@@ -1,6 +1,6 @@
 export function isRGBColor(input: string) {
 	const regex = /rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)$/;
-	if (regex.exec(input) == null) {
+	if (regex.exec(input) === null) {
 		console.warn('Input must be rgb color');
 	}
 }
