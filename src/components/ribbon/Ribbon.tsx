dd1ad97a0ebd920b24170f66f11e6396b0445ff3
Michal Terczynski <mterczynski271@gmail.com>
2020-02-20 22:14:20 +0100

    Refactored Ribbon.tsx

--- src/components/ribbon/Ribbon.tsx
+++ src/components/ribbon/Ribbon.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainToolsTab from './main-tools-tab/MainToolsTab';
 import ViewTab from './view-tab/ViewTab';
 
@@ -9,94 +9,90 @@ enum Tabs {
 	View = 'View',
 }
 
-export default class Ribbon extends React.Component {
-	readonly state = {
-		activeTab: Tabs.MainTools,
-		isCollapsed: false,
+const HelpIcon = () => {
+	return <div className='Ribbon__head__icons__icon'>
+		<img
+			draggable='false'
+			className='Ribbon__head__icons__icon__image'
+			src={require('../../assets/icons/top/help.png')}
+		/>
+	</div>;
+};
+
+const Ribbon = () => {
+	const [activeTab, setActiveTab] = useState(Tabs.MainTools);
+	const [isCollapsed, setIsColapsed] = useState(false);
+
+	function toggleRibbon() {
+		setIsColapsed(!isCollapsed);
+	}
+
+	const RibbonToggler = () => {
+		return <div
+			className='Ribbon__head__icons__icon'
+			onClick={toggleRibbon}
+		>
+			<img
+				draggable='false'
+				className={
+					'Ribbon__head__icons__icon__image ' +
+					(isCollapsed
+						? 'Ribbon__head__icons__icon__image--reversed'
+						: '')
+				}
+				src={require('../../assets/icons/top/collapse.png')}
+			/>
+		</div>;
 	};
 
-	constructor(props) {
-		super(props);
-		this.toggleRibbon = this.toggleRibbon.bind(this);
-	}
+	return (
+		<div className='Ribbon'>
+			<div className='Ribbon__head'>
+				<div className='Ribbon__head__tabNames'>
+					<div className='Ribbon__head__tabNames__file'>Plik</div>
 
-	changeTab(tabName: Tabs) {
-		this.setState({ activeTab: tabName });
-	}
-
-	toggleRibbon() {
-		this.setState({ isCollapsed: !this.state.isCollapsed });
-	}
-
-	render() {
-		return (
-			<div className='Ribbon'>
-				<div className='Ribbon__head'>
-					<div className='Ribbon__head__tabNames'>
-						<div className='Ribbon__head__tabNames__file'>Plik</div>
-
-						<div
-							onClick={() => this.changeTab(Tabs.MainTools)}
-							className={
-								'Ribbon__head__tabNames__tab ' +
-								(this.state.activeTab === Tabs.MainTools
-									? 'Ribbon__head__tabNames__tab--active'
-									: '')
-							}
-						>
-							Narzędzia główne
+					<div
+						onClick={() => setActiveTab(Tabs.MainTools)}
+						className={
+							'Ribbon__head__tabNames__tab ' +
+							(activeTab === Tabs.MainTools
+								? 'Ribbon__head__tabNames__tab--active'
+								: '')
+						}
+					>
+						Narzędzia główne
 						</div>
 
-						<div
-							onClick={() => this.changeTab(Tabs.View)}
-							className={
-								'Ribbon__head__tabNames__tab ' +
-								(this.state.activeTab === Tabs.View
-									? 'Ribbon__head__tabNames__tab--active'
-									: '')
-							}
-						>
-							Widok
+					<div
+						onClick={() => setActiveTab(Tabs.View)}
+						className={
+							'Ribbon__head__tabNames__tab ' +
+							(activeTab === Tabs.View
+								? 'Ribbon__head__tabNames__tab--active'
+								: '')
+						}
+					>
+						Widok
 						</div>
-					</div>
-
-					<div className='Ribbon__head__icons'>
-						<div
-							className='Ribbon__head__icons__icon'
-							onClick={this.toggleRibbon}
-						>
-							<img
-								draggable='false'
-								className={
-									'Ribbon__head__icons__icon__image ' +
-									(this.state.isCollapsed
-										? 'Ribbon__head__icons__icon__image--reversed'
-										: '')
-								}
-								src={require('../../assets/icons/top/collapse.png')}
-							/>
-						</div>
-
-						<div className='Ribbon__head__icons__icon'>
-							<img
-								draggable='false'
-								className='Ribbon__head__icons__icon__image'
-								src={require('../../assets/icons/top/help.png')}
-							/>
-						</div>
-					</div>
 				</div>
 
-				{this.state.isCollapsed === false ? (
-					<div className='Ribbon__body'>
-						{this.state.activeTab === Tabs.MainTools ? (
-							<MainToolsTab />
-						) : (
+				<div className='Ribbon__head__icons'>
+					<RibbonToggler></RibbonToggler>
+					<HelpIcon></HelpIcon>
+				</div>
+			</div>
+
+			{isCollapsed === false ? (
+				<div className='Ribbon__body'>
+					{activeTab === Tabs.MainTools ? (
+						<MainToolsTab />
+					) : (
 							<ViewTab />
 						)}
-					</div>
-				) : null}
-			</div>
-		);
-	}
-}
+				</div>
+			) : null}
+		</div>
+	);
+};
+
+export default Ribbon;
