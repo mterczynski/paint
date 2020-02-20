74b20e50e8970b1c754aa0b19a6962662ace8d2d
Michal Terczynski <mterczynski271@gmail.com>
2020-02-20 21:40:58 +0100

    Refactored Paint3d.tsx

--- src/components/ribbon/main-tools-tab/groups/paint3d/Paint3d.tsx
+++ src/components/ribbon/main-tools-tab/groups/paint3d/Paint3d.tsx
@@ -1,39 +1,21 @@
 import React from 'react';
-import { connect } from 'react-redux';
-
 import './Paint3d.scss';
 
-const mapStateToProps = state => {
-	return {
-	};
-};
+const Paint3d = () => {
+	return (
+		<div className='Paint3d'>
+			<div className='Paint3d__content'>
+				<img
+					draggable='false'
+					className='Paint3d__mainIcon'
+					src={require('../../../../../assets/icons/main-tools-tab/8_paint3d.png')}
+					alt=''
+				/>
 
-class Paint3dComponent extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isListCollapsed: false,
-		};
-	}
-
-	render() {
-		return (
-			<div className='Paint3d'>
-				<div className='Paint3d__content'>
-					<img
-						draggable='false'
-						className='Paint3d__mainIcon'
-						src={require('../../../../../assets/icons/main-tools-tab/8_paint3d.png')}
-						alt=''
-					/>
-
-					<div className='Paint3d__description'>Otwórz aplikację Paint 3D</div>
-				</div>
+				<div className='Paint3d__description'>Otwórz aplikację Paint 3D</div>
 			</div>
-		);
-	}
-}
-
-const Paint3d = connect(mapStateToProps)(Paint3dComponent);
+		</div>
+	);
+};
 
 export default Paint3d;
