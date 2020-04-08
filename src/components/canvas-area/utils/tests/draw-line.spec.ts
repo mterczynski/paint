a4d6f57bea6c81f700c3418f658a831e30eaffa0
Michal Terczynski <mterczynski271@gmail.com>
2020-04-08 18:51:59 +0200

    Added drawLine test

---
+++ src/components/canvas-area/utils/tests/draw-line.spec.ts
@@ -0,0 +1,14 @@
+import { drawLine } from '../draw-line';
+
+describe('drawLine', () => {
+test('should execute without error', () => {
+			const canvas = document.createElement('canvas');
+
+			const result = drawLine({
+				color: 'black',
+				context: canvas.getContext('2d') as CanvasRenderingContext2D,
+				from: {x: 10, y:20},
+				to: {x: 20, y:30}
+			});
+	});
+});
