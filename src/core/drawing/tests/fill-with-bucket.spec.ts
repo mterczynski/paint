70f2c62bbafdd68bbfee90c4740a9774efbca819
Michal Terczynski <mterczynski271@gmail.com>
2020-04-11 16:45:14 +0200

    temp: impl of fill-with-bucket using manipulation of imageData

--- src/core/drawing/tests/fill-with-bucket.spec.ts
+++ src/core/drawing/tests/fill-with-bucket.spec.ts
@@ -2,18 +2,31 @@ import { fillWithBucket } from '../fill-with-bucket';
 
 describe('fillWithBucket', () => {
 	test('should fill whole canvas with one color if canvas is blank', () => {
+		const width = 5;
+		const height = 2;
+
 		const mockCanvas = document.createElement('canvas');
-		mockCanvas.width = 5;
-		mockCanvas.height = 2;
+		mockCanvas.width = width;
+		mockCanvas.height = height;
 		const context = mockCanvas.getContext('2d') as CanvasRenderingContext2D;
+		const fillColor = {
+			red: 7,
+			green: 10,
+			blue: 20,
+			alpha: 128
+		};
 
 		fillWithBucket({
 			context,
-			fillColor: 'rgb(7, 10, 20)',
-			mousePositionRelativeToCanvas: {x: 0, y: 0}
+			fillColor,
+			mousePositionRelativeToCanvas: {x: 0, y: 0},
+			canvasSize: {
+				width,
+				height
+			}
 		});
 
-		const pixelData = Array.from(context.getImageData(0, 0, 100, 100).data);
+		const pixelData = Array.from(context.getImageData(0, 0, width, height).data);
 
 		expect(pixelData).toEqual([
 			7, 10, 20, 128,
