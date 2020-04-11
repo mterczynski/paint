76cc0dcac2f9ab9c4e8f3328f155943c9b65eb93
Michal Terczynski <mterczynski271@gmail.com>
2020-04-11 13:01:57 +0200

    !9 temp changes

--- src/core/drawing/fill-with-bucket.ts
+++ src/core/drawing/fill-with-bucket.ts
@@ -1,21 +1,31 @@
 import { Point } from '../../types';
+import { getPixelColor } from './utils';
+
+interface RectangleSize {
+	height: number,
+	width: number
+}
 
 interface FillWithBucketArgs {
 	context: CanvasRenderingContext2D,
 	mousePositionRelativeToCanvas: Point,
-	canvasSize: {
-		height: number,
-		width: number
-	}
+	canvasSize: RectangleSize,
 	fillColor: string
 }
 
 export function fillWithBucket({context, fillColor, canvasSize, mousePositionRelativeToCanvas}: FillWithBucketArgs) {
-	const imageData = context.getImageData(0, 0, canvasSize.width, canvasSize.height).data;
-	const imageDataPixels = imageData.join(',')
-		.replace(/(\d+,\d+,\d+,\d+),/g, '$1x')
-		.split('x')
-		.map(subArray => subArray.split(',').map(value => Number(value)));
 
-	context.putImageData(imageData);
+	const imageData = context.getImageData(0, 0, canvasSize.width, canvasSize.height).data;
+	// const imageDataPixels = imageData.join(',')
+	// 	.replace(/(\d+,\d+,\d+,\d+),/g, '$1x')
+	// 	.split('x')
+	// 	.map(subArray => subArray.split(',').map(value => Number(value)));
+
+	const startX = mousePositionRelativeToCanvas.x;
+	const startY = mousePositionRelativeToCanvas.y;
+
+	const openList: {x: number, y: number}[] = [];
+	const closedList: {x: number, y: number}[] = [];
+
+	// const clickedPixelColor = getPixelColor({...mousePositionRelativeToCanvas, imageSize: canvasSize, imageData});
 }
