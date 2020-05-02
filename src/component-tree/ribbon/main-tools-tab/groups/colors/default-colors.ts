6fc55bc6f3db532ce9a847fcb6c68e0845d72eb0
Michal Terczynski <mterczynski271@gmail.com>
2020-05-02 12:01:38 +0200

    Refactor: moved default-colors to separate file

---
+++ src/component-tree/ribbon/main-tools-tab/groups/colors/default-colors.ts
@@ -0,0 +1,28 @@
+export const defaultColors = Object.freeze([
+	// first row:
+	Object.freeze([
+		{red: 0, green: 0, blue: 0, alpha: 255},
+		{red: 127, green: 127, blue: 127, alpha: 255},
+		{red: 136, green: 0, blue: 21, alpha: 255},
+		{red: 237, green: 28, blue: 36, alpha: 255},
+		{red: 255, green: 127, blue: 39, alpha: 255},
+		{red: 255, green: 242, blue: 0, alpha: 255},
+		{red: 34, green: 177, blue: 76, alpha: 255},
+		{red: 0, green: 162, blue: 232, alpha: 255},
+		{red: 63, green: 72, blue: 204, alpha: 255},
+		{red: 163, green: 73, blue: 127, alpha: 255},
+	]),
+	// second row:
+	Object.freeze([
+		{red: 255, green: 255, blue: 255, alpha: 255},
+		{red: 195, green: 195, blue: 195, alpha: 255},
+		{red: 185, green: 122, blue: 87, alpha: 255},
+		{red: 255, green: 174, blue: 201, alpha: 255},
+		{red: 255, green: 201, blue: 14, alpha: 255},
+		{red: 239, green: 228, blue: 176, alpha: 255},
+		{red: 181, green: 230, blue: 29, alpha: 255},
+		{red: 153, green: 217, blue: 234, alpha: 255},
+		{red: 112, green: 146, blue: 190, alpha: 255},
+		{red: 200, green: 191, blue: 231, alpha: 255},
+	]),
+]);
