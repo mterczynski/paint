9fd9dc6f9eedcaa699fc0069a4503ceab65332f1
Michal Terczynski <mterczynski271@gmail.com>
2020-05-03 10:04:30 +0200

    Added en.lang

---
+++ src/lang/impl/en.lang.ts
@@ -0,0 +1,61 @@
+import { Lang } from '../lang.type';
+
+export const EN_Lang: Lang = Object.freeze({
+	homeTabs: {
+		clipboard: {
+			title: 'Clipboard',
+			paste: {
+				title: 'Paste',
+			},
+			copy: {
+				title: 'Copy',
+			},
+			cut: {
+				title: 'Cut',
+			},
+		},
+		image: {
+			select: {
+				title: 'Select',
+			},
+			crop: {
+				title: 'Crop',
+			},
+			resize: {
+				title: 'Resize',
+			},
+			rotate: {
+				title: 'Rotate',
+			},
+			title: 'Image',
+		},
+		tools: {
+			title: 'Tools',
+		},
+		brushes: {
+			title: 'Brushes',
+		},
+		shapes: {
+			title: 'Shapes',
+			outline: {
+				title: 'Outline',
+			},
+			fill: {
+				title: 'Fill',
+			},
+		},
+		size: {
+			title: 'Size',
+		},
+		colors: {
+			title: 'Colours',
+			color: 'Colour',
+			editColors: {
+				title: 'Edit colours',
+			},
+		},
+		paint3d: {
+			title: 'Edit with Paint 3D',
+		},
+	},
+});
--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,7 +1,9 @@
 import { PL_Lang } from './impl/pl.lang';
+import { EN_Lang } from './impl/en.lang';
 
 export * from './lang.type';
 
 export const languages = Object.freeze({
+	EN: EN_Lang,
 	PL: PL_Lang,
 });
