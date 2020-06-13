c9c0d8a8b4be762debb0c250db853fafb18632a4
Michal Terczynski <mterczynski271@gmail.com>
2020-06-13 18:36:19 +0200

    Implemented color picker cursor

--- src/component-tree/canvas-area/cursors.ts
+++ src/component-tree/canvas-area/cursors.ts
@@ -5,6 +5,7 @@ const cursorYOffset = 20;
 const cursors = {
 	pencil: `url(${require('../../assets/cursors/pencil.png')}) 0 ${cursorYOffset}, auto`,
 	bucket: `url(${require('../../assets/cursors/bucket.png')}) 0 ${cursorYOffset}, auto`,
+	colorPicker: `url(${require('../../assets/cursors/color-picker.png')}) 0 ${cursorYOffset}, auto`,
 	default: 'default',
 };
 
@@ -13,6 +14,8 @@ export function getCursorForTool(tool: AvailableTools) {
 		return cursors.pencil;
 	} else if(tool === AvailableTools.Fill) {
 		return cursors.bucket;
+	} else if(tool === AvailableTools.ColorPicker) {
+		return cursors.colorPicker;
 	}  else {
 		return cursors.default;
 	}
