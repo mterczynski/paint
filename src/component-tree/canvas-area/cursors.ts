2adb510cd7e6c5faf089e9cab6a16567ebac99d9
Michal Terczynski <mterczynski271@gmail.com>
2020-06-13 18:32:04 +0200

    Added bucket cursor

--- src/component-tree/canvas-area/cursors.ts
+++ src/component-tree/canvas-area/cursors.ts
@@ -4,13 +4,16 @@ const cursorYOffset = 20;
 
 const cursors = {
 	pencil: `url(${require('../../assets/cursors/pencil.png')}) 0 ${cursorYOffset}, auto`,
+	bucket: `url(${require('../../assets/cursors/bucket.png')}) 0 ${cursorYOffset}, auto`,
 	default: 'default',
 };
 
 export function getCursorForTool(tool: AvailableTools) {
 	if(tool === AvailableTools.Pencil) {
 		return cursors.pencil;
-	} else {
+	} else if(tool === AvailableTools.Fill) {
+		return cursors.bucket;
+	}  else {
 		return cursors.default;
 	}
 }
