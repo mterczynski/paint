e93990c6f591785155ef973215b309eb104d1493
mterczynski <mterczynski271@gmail.com>
2020-08-16 18:45:42 +0200

    [Refactor] Changed ToolSize to enum

--- src/redux/initial-state.ts
+++ src/redux/initial-state.ts
@@ -1,13 +1,13 @@
 import { languages } from '../lang/impl';
-import { AppState, AvailableTools, Tabs, MouseButton } from '../types';
+import { AppState, AvailableTools, Tabs, MouseButton, ToolSize } from '../types';
 import { Dropdowns } from '../types';
 
 export const initialState: AppState = {
 	activeTab: Tabs.MainTools,
 	canvasContext: null,
 	colors: {
-		color1: {red: 0, green: 0, blue: 0, alpha: 255},
-		color2: {red: 255, green: 255, blue: 255, alpha: 255},
+		color1: { red: 0, green: 0, blue: 0, alpha: 255 },
+		color2: { red: 255, green: 255, blue: 255, alpha: 255 },
 		selectedMainColorIndex: 1,
 		lastUsedCustomColors: new Array(10).fill(null),
 		isEditColorsWindowOpened: false,
@@ -25,6 +25,6 @@ export const initialState: AppState = {
 	openedDropdown: Dropdowns.none,
 	preventNextAppClick: false,
 	selectedTool: AvailableTools.Pencil,
-	toolSize: 3,
+	toolSize: ToolSize.three,
 	zoom: 1,
 };
--- src/types/app-state.interface.ts
+++ src/types/app-state.interface.ts
@@ -5,7 +5,12 @@ import { Lang } from '../lang/types';
 
 export type AvailableZoomLevels = 0.125 | 0.25 | 0.5 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 export type IndexOfMainColor = 1 | 2;
-export type ToolSize = 1 | 2 | 3 | 4;
+export enum ToolSize {
+	one = 1,
+	two = 2,
+	three = 3,
+	four = 4
+}
 
 export enum Tabs {
 	MainTools = 'MainTools',
