e2feeb03be8612ac91bd150cfdad01baf98f303a
Michał Terczyński <mterczynski271@gmail.com>
2021-12-25 20:13:27 +0100

    Implement crosshair moves

--- src/component-tree/popups/edit-colours/color-picker-column/HueAndSaturationPicker.tsx
+++ src/component-tree/popups/edit-colours/color-picker-column/HueAndSaturationPicker.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { Crosshair } from "./Crosshair";
 
 const assets = {
@@ -5,14 +6,51 @@ const assets = {
 };
 
 export const HueAndSaturationPicker = () => {
+	const imageRef = useRef<HTMLImageElement>(null);
+	const [crosshairPosition, setCrosshairPosition] = useState({x: 0, y: 0});
+	const [activeMouseMoveListener, setActiveMouseMoveListener] = useState<React.MouseEventHandler<HTMLImageElement> | undefined>(undefined);
+
+	const updateCrosshairPosition = (mouseEvent: React.MouseEvent) => {
+		const imagePosition = imageRef.current?.getBoundingClientRect();
+
+		if(!imagePosition) {
+			return;
+		}
+
+		setCrosshairPosition({
+			x: mouseEvent.pageX - imagePosition.left,
+			y: mouseEvent.pageY - imagePosition.top
+		});
+	}
+
+	const mouseMoveListener = (mouseEvent: React.MouseEvent) => {
+		updateCrosshairPosition(mouseEvent);
+	}
+
+	const onMouseDown = (mouseEvent: React.MouseEvent) => {
+		updateCrosshairPosition(mouseEvent);
+		setActiveMouseMoveListener(() => mouseMoveListener);
+	}
+
+	useEffect(function setUpMouseListener() {
+		const mouseUpListener = () => {
+			setActiveMouseMoveListener(undefined);
+		}
+		window.addEventListener('mouseup', mouseUpListener);
+		return () => {
+			window.removeEventListener('mouseup', mouseUpListener)
+		}
+	}, []);
+
 	return <div style={{
 		width: '175px',
 		height: '187px',
 		overflow: 'hidden',
 		position: 'relative'
 	}}>
-		<Crosshair/>
+		<Crosshair position={{x: crosshairPosition.x, y: crosshairPosition.y}}/>
 		<img
+			ref={imageRef}
 			width='100%'
 			height='100%'
 			style={{
@@ -23,6 +61,8 @@ export const HueAndSaturationPicker = () => {
 			alt=''
 			draggable='false'
 			src={assets.hueAndSaturationPicker}
+			onMouseDown={onMouseDown}
+			onMouseMove={activeMouseMoveListener}
 		></img>
 	</div>;
 };
