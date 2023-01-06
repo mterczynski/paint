d5ac28f754eee2f1c4f40e38fd71768240102068
Michał Terczyński <mterczynski1@gmail.com>
2023-01-06 18:52:52 +0100

    Fix cursors.ts

--- src/component-tree/canvas-area/cursors.ts
+++ src/component-tree/canvas-area/cursors.ts
@@ -1,21 +1,25 @@
 import { AvailableTools } from "../../types";
 
+import pencil from '/assets/cursors/pencil.png'
+import bucket from '/assets/cursors/bucket.png'
+import colorPicker from '/assets/cursors/color-picker.png'
+
 const cursorYOffset = 20;
 
 const cursors = {
-	// pencil: `url(${require("../../assets/cursors/pencil.png")}) 0 ${cursorYOffset}, auto`,
-	// bucket: `url(${require("../../assets/cursors/bucket.png")}) 0 ${cursorYOffset}, auto`,
-	// colorPicker: `url(${require("../../assets/cursors/color-picker.png")}) 0 ${cursorYOffset}, auto`,
+	pencil: `url(${pencil}) 0 ${cursorYOffset}, auto`,
+	bucket: `url(${bucket}) 0 ${cursorYOffset}, auto`,
+	colorPicker: `url(${colorPicker}) 0 ${cursorYOffset}, auto`,
 	default: "default",
 };
 
 export function getCursorForTool(tool: AvailableTools) {
 	if (tool === AvailableTools.Pencil) {
-		return cursors.default;
+		return cursors.pencil;
 	} else if (tool === AvailableTools.Fill) {
-		return cursors.default;
+		return cursors.bucket;
 	} else if (tool === AvailableTools.ColorPicker) {
-		return cursors.default;
+		return cursors.colorPicker;
 	} else {
 		return cursors.default;
 	}
